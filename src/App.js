3c453838ab7300f57c52a3acf66a2fa02f64fbd9
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-24 22:37:48 -0700

    initial input conversion working

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
   const [fromCurrency, setFromCurrency] = useState();
   const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
 
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
@@ -17,6 +28,7 @@ function App() {
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
         setFromCurrency(data.base);
         setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
       });
   }, []);
 
@@ -26,13 +38,15 @@ function App() {
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency={fromCurrency}
-        onChangeCurrency={e => setFromCurrency(e.target.value)}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        amount={fromAmount}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency={toCurrency}
-        onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        amount={toAmount}
       />
     </>
   );
