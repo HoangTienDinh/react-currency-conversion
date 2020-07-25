e6a774139067e803bf7ce3f721873ecd979ea24a
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-24 22:12:13 -0700

    base layout done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import CurrencyRow from "./CurrencyRow";
 
 function App() {
-  return <div className="App">Hello Hoang</div>;
+  return (
+    <>
+      <div className="App">Convert</div>
+      <CurrencyRow />
+        <div>=</div>
+      <CurrencyRow />
+    </>
+  );
 }
 
 export default App;
